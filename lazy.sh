1bc66d2d5cf63dd7e3526cd3e44d5f7995a376f9
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2024-04-18 11:58:23 +0200

    🚀

--- lazy.sh
+++ lazy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-open -a firefox --new-window 'http://localhost:4000/'
+open -a firefox -n 'http://localhost:4000/'
 open -a firefox 'https://getbootstrap.com/docs/5.3/getting-started/introduction/'
 open -a firefox 'https://github.com/RawWorkz/raw-genius-2024'
 
