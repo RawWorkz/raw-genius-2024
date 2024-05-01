6ccb7bde182f1d5294d722836d70a8c073b2c587
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2024-05-01 16:08:46 +0200

    better toggle fullscreen

--- src/app/pages/screensaver-page/screensaver-page.component.ts
+++ src/app/pages/screensaver-page/screensaver-page.component.ts
@@ -9,16 +9,27 @@ export class ScreensaverPageComponent implements OnInit {
 
   title = 'RawGenius Screensaver';
 
+  isFullscreen: boolean = false;
+
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // Listen for fullscreen change event
+    document.addEventListener('fullscreenchange', () => {
+      this.isFullscreen = !!window.document.fullscreenElement;
+    });
+  }
 
   onFullScreenClickHandler() {
     console.log(window.document.fullscreenEnabled);
     console.log(window.document.fullscreenElement);
+
+
     if (!window.document.fullscreenElement) {
+      this.isFullscreen = true;
       document.body.requestFullscreen();
     } else {
+      this.isFullscreen = false;
       window.document.exitFullscreen();
     }
   }
